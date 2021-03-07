ad1f9510693d1a8571ad713bff73ee2a1987f6c1
Oscar De La Lanza <oscardelalanza@gmail.com>
2021-03-06 20:57:06 -0800

    owner migration added and devise configured to authenticate though phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_07_035009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.integer "available_day", default: 0, null: false
+    t.string "hour_start", default: "", null: false
+    t.string "hour_end", default: "", null: false
+    t.string "jti", null: false
+    t.string "phone", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["jti"], name: "index_owners_on_jti", unique: true
+    t.index ["phone"], name: "index_owners_on_phone", unique: true
+    t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  end
 
 end

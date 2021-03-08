bb04aec34be894ef203735da8bdd316f2b3351e1
Oscar De La Lanza <oscardelalanza@gmail.com>
2021-03-08 04:21:16 -0800

    properties update added

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,12 +1,12 @@
 class PropertiesController < ApplicationController
   before_action :authenticate_owner!
+  before_action :find_property, only: %i[show update delete]
 
   def index
     @properties = current_owner.properties
   end
 
   def show
-    @property = Property.find_by(id: params[:id], owner_id: current_owner.id)
     if @property
       render :show, status: :ok
     else
@@ -16,14 +16,23 @@ class PropertiesController < ApplicationController
 
   def create
     @property = current_owner.properties.build(property_params)
+
     if @property.save
       render :show, status: :created
     else
-      render json: @property.errors, status: :unprocessable_entity
+      render_errors
     end
   end
 
-  def update; end
+  def update
+    @property&.update(property_params)
+
+    if @property.save
+      render :show, status: :created
+    else
+      render_errors
+    end
+  end
 
   def destroy; end
 
@@ -31,5 +40,13 @@ class PropertiesController < ApplicationController
 
   def property_params
     params.require(:property).permit(:name, :description, :status, :rental_price)
+  end
+
+  def find_property
+    @property = Property.find_by(id: params[:id], owner_id: current_owner.id)
+  end
+
+  def render_errors
+    render json: @property.errors, status: :unprocessable_entity
   end
 end

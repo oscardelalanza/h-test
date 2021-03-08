aad23b6371dddfdc37340f5c8024df49de6f537a
Oscar De La Lanza <oscardelalanza@gmail.com>
2021-03-08 05:16:13 -0800

    parnter validations and tests

--- test/models/partner_test.rb
+++ test/models/partner_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class PartnerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @partner = Partner.new(name: 'The partner')
+  end
+
+  test 'partner saves correctly' do
+    assert @partner.save
+  end
+
+  test 'partner has a valid name' do
+    @partner.name = ''
+    assert_not @partner.save
+    @partner.name = 'random partner'
+    assert @partner.save
+  end
+
+  test 'partner has a token' do
+    assert_not @partner.auth_token
+    @partner.save
+    assert @partner.auth_token
+  end
 end
